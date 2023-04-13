78168c1c1a1b6bfe368b45d2c764ddbdc6060f14
Isaac Adeni Adariku <isaacadariku05@gmail.com>
2023-04-13 16:12:17 +0100

    ✅ add pigeon package and generate files

--- flutter_native_badge_foundation/lib/flutter_native_badge_foundation.dart
+++ flutter_native_badge_foundation/lib/flutter_native_badge_foundation.dart
@@ -1,8 +1,1 @@
-
-import 'flutter_native_badge_foundation_platform_interface.dart';
-
-class FlutterNativeBadgeFoundation {
-  Future<String?> getPlatformVersion() {
-    return FlutterNativeBadgeFoundationPlatform.instance.getPlatformVersion();
-  }
-}
+class FlutterNativeBadgeFoundation {}
---
+++ flutter_native_badge_foundation/pigeons/flutter_native_badge_api.dart
@@ -0,0 +1,17 @@
+import 'package:pigeon/pigeon.dart';
+
+@ConfigurePigeon(
+  PigeonOptions(
+    input: 'pigeons/flutter_native_badge_api.dart',
+    dartOut: 'lib/flutter_native_badge_api.g.dart',
+    swiftOut: 'darwin/Classes/FlutterNativeBadge.g.swift',
+  ),
+)
+@HostApi()
+abstract class FlutterNativeBadgeApi {
+  int getBadgeCount();
+
+  void setBadgeCount(int count);
+
+  void resetBadgeCount();
+}
